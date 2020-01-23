5eba65d10b1e191038b66330ca0358861e8d0dc4
yuecheng <176547352@qq.com>
2020-01-23 14:32:42 +0800

    增加pregnanthelper

--- Android/NanoSparrow/app/src/main/java/com/nanosparrow/nanosparrow/ui/home/HomeFragment.kt
+++ Android/NanoSparrow/app/src/main/java/com/nanosparrow/nanosparrow/ui/home/HomeFragment.kt
@@ -1,14 +1,15 @@
 package com.nanosparrow.nanosparrow.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.nanosparrow.nanosparrow.R
+import com.nanosparrow.PregnantAssistant.ExpectedDateConfinementActivity
 
 class HomeFragment : Fragment() {
 
@@ -22,6 +23,11 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val testButton = root.findViewById<Button>(R.id.test_button)
+        testButton.setOnClickListener {
+            val intent = Intent(context, ExpectedDateConfinementActivity::class.java)
+            startActivity(intent)
+        }
         return root
     }
 }
