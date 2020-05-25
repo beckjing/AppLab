75b9acba0f435e1ced5d665f8a5ee0bf8e84b68c
yuecheng <176547352@qq.com>
2020-05-25 11:15:38 +0800

    no message

---
+++ iOS/NanoSparrow/Pods/Target Support Files/Pods-NanoSparrow-NanoSparrowUITests/Pods-NanoSparrow-NanoSparrowUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NanoSparrow_NanoSparrowUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NanoSparrow_NanoSparrowUITests
+@end
---
+++ iOS/NanoSparrow/Pods/Target Support Files/Pods-NanoSparrow/Pods-NanoSparrow-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NanoSparrow : NSObject
+@end
+@implementation PodsDummy_Pods_NanoSparrow
+@end
